a04a2357a5a14ed23cf5baedcc478ff92f50ade7
FerdaKoplo <sahmuraivan43@gmail.com>
2026-01-29 21:00:48 +0700

    add pdf upload endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, UploadFile, File, BackgroundTasks
 from sqlalchemy.orm import Session
 from app.db.session import SessionLocal
 from app.domain.services.decision_engine import evaluate_decision
+from app.domain.services.document_processor import process_and_save_document
 
 app = FastAPI()
 
@@ -18,3 +19,14 @@ def get_db():
 async def get_decision(decision_id: int, db: Session = Depends(get_db)):
     result = await evaluate_decision(decision_id, db)
     return result
+
+
+@app.post("/documents/upload")
+async def upload_document(
+    background_tasks: BackgroundTasks,
+    file: UploadFile = File(...),
+    db: Session = Depends(get_db),
+):
+    background_tasks.add_task(process_and_save_document, db, file)
+
+    return {"filename": file.filename, "status": "processing_started"}
